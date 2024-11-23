58b46452855d8296416eb88716f248ee0dc0b990
Giovani Betini <gio.betini@gmail.com>
2024-11-23 13:04:55 -0300

    Cria página de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import {ChangeDetectionStrategy} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatTooltip} from '@angular/material/tooltip';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormControl, Validators, ReactiveFormsModule, FormsModule} from '@angular/forms';
+import { MyErrorStateMatcher } from '../../helpers/EmailErrorStateMatcher';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, MatFormFieldModule, MatInputModule, MatSelectModule, ReactiveFormsModule, FormsModule, MatButtonModule, MatIconModule, MatTooltip],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  matcher = new MyErrorStateMatcher();
+  hide = signal(true);
+  isDisabled = true;
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide())
+    event.stopPropagation();
+  }
+}
