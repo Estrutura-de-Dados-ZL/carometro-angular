561753cf5687280c66c3f2a30622386f6a7826ad
g4bri0s <g.cristianods@gmail.com>
2024-11-25 14:33:07 -0300

    add turma component and service

--- src/app/services/turma.service.ts
+++ src/app/services/turma.service.ts
@@ -1,9 +1,60 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ITurma, ITurmaId } from '../interfaces/turma';
+import { ApiRespostaDto } from '../interfaces/api-resposta-dto';
+
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TurmaService {
+  private readonly API_URL = 'http://localhost:8080/turma';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  /**
+   * Busca todas as turmas.
+   */
+  listarTurmas(): Observable<ITurma[]> {
+    return this.http.get<ITurma[]>(`${this.API_URL}/buscarTodos`);
+  }
+
+  /**
+   * Busca uma turma pelo ID composto (cursoId, ano, semestre).
+   * @param turmaId Identificador composto da turma.
+   */
+  buscarTurmaPorId(turmaId: ITurmaId): Observable<ApiRespostaDto<ITurma>> {
+    const { cursoId, ano, semestre } = turmaId;
+    return this.http.get<ApiRespostaDto<ITurma>>(
+      `${this.API_URL}/${cursoId}/${ano}/${semestre}`
+    );
+  }
+
+  /**
+   * Cria uma nova turma.
+   * @param turma Dados da turma.
+   */
+  criarTurma(turma: ITurma): Observable<string> {
+    return this.http.post<string>(this.API_URL, turma);
+  }
+
+  /**
+   * Atualiza uma turma existente.
+   * @param turma Dados da turma atualizada.
+   */
+  atualizarTurma(turma: ITurma): Observable<string> {
+    return this.http.put<string>(this.API_URL, turma);
+  }
+
+  /**
+   * Deleta uma turma pelo ID composto (cursoId, ano, semestre).
+   * @param turmaId Identificador composto da turma.
+   */
+  deletarTurma(turmaId: ITurmaId): Observable<string> {
+    const { cursoId, ano, semestre } = turmaId;
+    return this.http.delete<string>(
+      `${this.API_URL}/${cursoId}/${ano}/${semestre}`
+    );
+  }
 }
