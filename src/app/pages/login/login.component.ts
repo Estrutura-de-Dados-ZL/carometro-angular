f29f2cfefddc1bff1b5e761d5c132641733173df
Giovani Betini <gio.betini@gmail.com>
2024-12-04 20:10:00 -0300

    Aplicação de CSS na tela de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
@@ -8,4 +8,35 @@ import { NavbarComponent } from '../../components/navbar/navbar.component';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  passwordFormControl = new FormControl('', [Validators.required, Validators.minLength(8)]);
+  emailFormControlSignIn = new FormControl('', [Validators.required, Validators.email]);
+  passwordFormControlSignIn = new FormControl('', [Validators.required, Validators.minLength(8)]);
+  matcher = new MyErrorStateMatcher();
+  hide = signal(true);
+  hideSignIn = signal(true)
+  isDisabled = true;
+  isDisabledSignIn = true;
+
+  highlightButtonLogIn(event: any){
+    if (this.emailFormControl.valid && this.passwordFormControl.valid){
+      this.isDisabled = false;
+    }
+  }
+  highlightButtonSignIn(event: any){
+    if (this.emailFormControl.valid && this.passwordFormControl.valid){
+      this.isDisabledSignIn = false;
+    }
+  }
+
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide())
+    event.stopPropagation();
+  }
+
+  clickEventSignIn(event: MouseEvent) {
+    this.hideSignIn.set(!this.hideSignIn())
+    event.stopPropagation();
+  }
+}
