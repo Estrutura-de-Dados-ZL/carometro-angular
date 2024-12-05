242a369e2359337519ef4cb9f0472f3e7531e2fb
g4bri0s <g.cristianods@gmail.com>
2024-12-05 16:49:07 -0300

    fix

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,27 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import {ChangeDetectionStrategy} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatTooltip} from '@angular/material/tooltip';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormControl, Validators, ReactiveFormsModule, FormsModule} from '@angular/forms';
+import { MyErrorStateMatcher } from '../../helpers/EmailErrorStateMatcher';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [
+    NavbarComponent,
+    MatFormFieldModule,
+    MatInputModule, MatSelectModule,
+    ReactiveFormsModule, FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltip],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
