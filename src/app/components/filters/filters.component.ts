44fe4f8c1025723d998f1e57773fd24288826195
deArrudal <arrudalucas@hotmail.com>
2024-12-06 14:51:01 -0300

    fix filtro para retornar cursos

--- src/app/components/filters/filters.component.ts
+++ src/app/components/filters/filters.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -6,21 +6,8 @@ import { MatSelectChange, MatSelectModule } from '@angular/material/select';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
-
-const cursosMock = [
-  {
-    nome: 'Analise e Desenvolvimento de Sistemas',
-    value: 1,
-  },
-  {
-    nome: 'Comex',
-    value: 1,
-  },
-  {
-    nome: 'Polimeros',
-    value: 1,
-  },
-];
+import { CursoService } from '../../services/curso.service';
+import { ICurso } from '../../interfaces/curso'; 
 
 export interface IFilter {
   ano: number;
@@ -49,11 +36,12 @@ export interface ISemestre {
   templateUrl: './filters.component.html',
   styleUrl: './filters.component.css',
 })
-export class FiltersComponent {
+
+export class FiltersComponent implements OnInit {
   @Output() enviarFiltro = new EventEmitter<IFilter>();
   @Output() limparFiltro = new EventEmitter<null>();
 
-  cursos = cursosMock;
+  cursos: ICurso[] = []; // Initialize an empty array for courses
   selectedCurso: number = 0;
   aluno: string = '';
   ano!: number | null;
@@ -69,6 +57,24 @@ export class FiltersComponent {
       id: 2,
     },
   ];
+
+  constructor(private cursoService: CursoService) { }
+
+  ngOnInit(): void {
+    this.loadCursos(); // Call loadCursos when the component initializes
+  }
+
+  // Method to load courses from the service
+  loadCursos(): void {
+    this.cursoService.listarCursos().subscribe(
+      (cursos) => {
+        this.cursos = cursos; // Assign fetched courses to the cursos array
+      },
+      (error) => {
+        console.error('Error loading courses', error);
+      }
+    );
+  }
 
   onAnoChange() {
     if (this.ano !== null) {
